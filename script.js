3600bb25bd485443203e079958c751f1425b700e
Jakub Antonik <https://github.com/jakubantonik>
2023-04-06 17:36:30 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://tasks-1.jakubantonik.repl.co/v1/tasks';
+  var apiRoot = 'https://tasks-1.jakubantonik.repl.co;
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
